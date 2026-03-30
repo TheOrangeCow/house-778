5ccb16f1e3cd15ca64902ccc5b298cfa0d2cbf6d
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-03-30 12:26:03 +0100

    Update chech.php

--- base/chech.php
+++ base/chech.php
@@ -1,5 +1,12 @@
 <?php
-session_set_cookie_params(0, '/', 'house-778.theorangecow.org');
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'domain' => '.theorangecow.org', 
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
 session_start();
 
 if (!isset($_SESSION["user_id"])) {
